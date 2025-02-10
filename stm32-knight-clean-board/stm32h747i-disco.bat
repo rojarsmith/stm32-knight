c24c3b3591229ba81d6f1114ac7dc206e2dfb23c
Rojar Smith <rojarsmith@gmail.com>
2025-02-10 22:01:11 +0800

    feat: Clean All Storage for STM32H747I-DISCO

---
+++ stm32-knight-clean-board/stm32h747i-disco.bat
@@ -0,0 +1,5 @@
+STM32_Programmer_CLI -c port=SWD -e all
+
+STM32_Programmer_CLI -c port=SWD -e all -el "%STM32CLT_PATH%\STM32CubeProgrammer\bin\ExternalLoader\MT25TL01G_STM32H747I-DISCO.stldr"
+
+pause
